c089f32323155bc2331dcb093debb124cfcec071
cemolcay <ccemolcay@gmail.com>
2015-08-17 22:56:35 +0300

    Initial Commit

---
+++ WalkerFixer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WalkerFixer
+//
+//  Created by Cem Olcay on 17/08/15.
+//  Copyright (c) 2015 prototapp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
